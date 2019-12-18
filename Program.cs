eacf61bb2c1f2126d16a1a4e278597a1653b31fb
Joeliton C.F <joeliton-ge@hotmail.com>
2019-12-17 22:25:43 -0300

    Melhorando a implementacao do metodo imprimir tabuleiro

--- Program.cs
+++ Program.cs
@@ -5,18 +5,18 @@ using xadres;
 namespace xadrex_console
 {
     class Program
-    {
+    {   
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8,8);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
             tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
             tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 2));
+
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 0));
 
             Tela.imprimirTabuleiro(tab);
-
-            Console.ReadLine();
         }
     }
 }
